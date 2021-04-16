c6428441d9ea9a892d010e454060694262650756
didinele <didinele.dev@gmail.com>
2021-04-16 21:49:41 +0300

    chore: update to 0.2.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { RoutingClient, createAmqp } from '@cordis/brokers';
-import { buildRestRouter } from '@cordis/routers';
-import { RestManager } from '@cordis/rest';
+import { Rest, buildRestRouter } from '@cordis/rest';
 import { GatewayDispatchEvents } from 'discord-api-types/v8';
 import type { DiscordEvents } from '@cordis/common';
 
 const main = async () => {
-  const manager = new RestManager(process.env.CORDIS_AUTH!);
+  const manager = new Rest(process.env.CORDIS_AUTH!);
   const router = buildRestRouter(manager);
 
   const { channel } = await createAmqp(process.env.CORDIS_AMQP_HOST!);
